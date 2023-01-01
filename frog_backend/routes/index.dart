f4353bddd93ce6ef79075275539d4c23859660c1
Vidiic <jordividal1997@gmail.com>
2023-01-01 22:45:30 +0100

    basic routes

--- frog_backend/routes/index.dart
+++ frog_backend/routes/index.dart
@@ -1,14 +1,34 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  bool respJson = [true, false].first;
   final request = context.request; //RequestContext
   final method = request.method.name; //POST, GET, DELETE ...
+  dynamic body;
+
+  //Headers
   final headers = request.headers;
-  String host = headers['host'] ?? 'test'; //Map<String, String>
+  final contentType = headers['content-type'] ?? 'test'; //Map<String, String>
+
+  //Params
   final params = request.uri.queryParameters; //Map<String, String>
-  String name = params['name'] ?? 'there';
-  final body = await request.body();//The request body can only be read once.
-  if(respJson) return Response.json(body: {'request_body': body});
-  return Response(body: 'Welcome to Dart Frog! with in $method method');
+  //final name = params['name'] ?? 'there';
+
+  switch (contentType) {
+    case 'application/x-www-form-urlencoded':
+      body = await request.formData();
+      break;
+    default:
+      body = await request.body(); //The request body can only be read once.
+      break;
+  }
+
+  return Response.json(
+    statusCode: 204, //Default 200
+    body: {
+      'request_body': body,
+      'params': params,
+      'method': method,
+      headers: {'hello': 'world'}
+    },
+  );
 }
