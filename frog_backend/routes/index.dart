7ea3c6ca5d2f73ba67e1404b97d24df0840b72da
Vidiic <jordividal1997@gmail.com>
2023-01-01 20:00:07 +0100

    basics

--- frog_backend/routes/index.dart
+++ frog_backend/routes/index.dart
@@ -1,5 +1,14 @@
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+Future<Response> onRequest(RequestContext context) async {
+  bool respJson = [true, false].first;
+  final request = context.request; //RequestContext
+  final method = request.method.name; //POST, GET, DELETE ...
+  final headers = request.headers;
+  String host = headers['host'] ?? 'test'; //Map<String, String>
+  final params = request.uri.queryParameters; //Map<String, String>
+  String name = params['name'] ?? 'there';
+  final body = await request.body();//The request body can only be read once.
+  if(respJson) return Response.json(body: {'request_body': body});
+  return Response(body: 'Welcome to Dart Frog! with in $method method');
 }
