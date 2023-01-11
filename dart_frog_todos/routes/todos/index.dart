97ecf14ff73e3bdadbe5299553ab55208ecc75ad
Vidiic <jordividal1997@gmail.com>
2023-01-11 20:09:38 +0100

    methods

--- dart_frog_todos/routes/todos/index.dart
+++ dart_frog_todos/routes/todos/index.dart
@@ -1,18 +1,31 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:todos_data_source/todos_data_source.dart';
 
-Future<Response> onRequest(RequestContext context) async  {
-  final request = context.request;
-  final method = request.method;
-  final data = await context.read<Future<TodosDataSource>>();
-  if(method == HttpMethod.get){
-    final todos = await data.readAll();
-    return Response.json(body: todos);
+Future<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context);
+    case HttpMethod.post:
+      return _post(context);
+    case HttpMethod.delete:
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.put:
+      return Response(body: 'Not found');
   }
-  if(method == HttpMethod.post){
-    final todo = Todo(title: 'HOPE');
-    await data.create(todo);
-    return Response(body: 'POST - TODOS');
-  }
-  return Response(body: 'No data');
+}
+
+Future<Response> _get(RequestContext context) async {
+  final provider = await context.read<Future<TodosDataSource>>();
+  final todos = await provider.readAll();
+  return Response.json(body: todos);
+}
+
+Future<Response> _post(RequestContext context) async {
+  final provider = await context.read<Future<TodosDataSource>>();
+  final params = context.request.uri.queryParameters;
+  final todo = Todo(title: params['title'] ?? '');
+  await provider.create(todo);
+  return Response.json(body: todo);
 }
