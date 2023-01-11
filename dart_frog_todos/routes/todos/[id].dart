9988a9dc1049f02c3125a4152d40e9dfd4648b65
Vidiic <jordividal1997@gmail.com>
2023-01-11 20:23:34 +0100

    todos id

--- dart_frog_todos/routes/todos/[id].dart
+++ dart_frog_todos/routes/todos/[id].dart
@@ -1,16 +1,34 @@
+import 'dart:html';
+
 import 'package:dart_frog/dart_frog.dart';
+import 'package:todos_data_source/todos_data_source.dart';
 
-Response onRequest(RequestContext context, String id) {
-  final request = context.request;
-  final method = request.method;
-
-  switch (method) {
+Future<Response> onRequest(RequestContext context, String id) async {
+  switch (context.request.method) {
     case HttpMethod.get:
-      return Response();
-    case HttpMethod.put:
-      return Response();
+      return _get(context, id);
+    case HttpMethod.post:
+      return _post(context, id);
     case HttpMethod.delete:
-      return Response();
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
   }
-  return Response();
+}
+
+Future<Response> _get(RequestContext context, String id) async {
+  final provider = await context.read<Future<TodosDataSource>>();
+  return Response.json(
+    body: await provider.read(id),
+  );
+}
+
+Future<Response> _post(RequestContext context, String id) async {
+  final provider = await context.read<Future<TodosDataSource>>();
+  //TODO UPDATE
+  return Response.json(
+    body: await provider.read(id),
+  );
 }
