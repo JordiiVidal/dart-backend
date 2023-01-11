9e205f5582a769abf88d5087c194c6e373944c81
Vidiic <jordividal1997@gmail.com>
2023-01-11 19:53:50 +0100

    Middleware datasource as provider

--- dart_frog_todos/README.md
+++ dart_frog_todos/README.md
@@ -1,10 +1,18 @@
-# dart_frog_todos
+# TODOS API
 
 [![style: very good analysis][very_good_analysis_badge]][very_good_analysis_link]
 [![License: MIT][license_badge]][license_link]
 [![Powered by Dart Frog](https://img.shields.io/endpoint?url=https://tinyurl.com/dartfrog-badge)](https://dartfrog.vgv.dev)
 
-An example application built with dart_frog
+## Packages
+- Data Source
+```dart
+An interface for a todos data source. Contains Models.
+```
+- Data Persistence
+```dart
+Implementation of data source in cache memory
+```
 
 [license_badge]: https://img.shields.io/badge/license-MIT-blue.svg
 [license_link]: https://opensource.org/licenses/MIT
--- dart_frog_todos/routes/todos/index.dart
+++ dart_frog_todos/routes/todos/index.dart
@@ -1,12 +1,17 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:todos_data_source/todos_data_source.dart';
 
-Response onRequest(RequestContext context) {
+Future<Response> onRequest(RequestContext context) async  {
   final request = context.request;
   final method = request.method;
+  final data = await context.read<Future<TodosDataSource>>();
   if(method == HttpMethod.get){
-     return Response(body: 'GET - TODOS');
+    final todos = await data.readAll();
+    return Response.json(body: todos);
   }
   if(method == HttpMethod.post){
+    final todo = Todo(title: 'HOPE');
+    await data.create(todo);
     return Response(body: 'POST - TODOS');
   }
   return Response(body: 'No data');
