3ce382745333aa1c4356225632d5c78380fc357c
Vidiic <jordividal1997@gmail.com>
2023-01-12 19:58:29 +0100

    rename ws example

--- dart_frog_web_socket_counter/example/main.dart
+++ dart_frog_web_socket_counter/example/main.dart
@@ -1,7 +1,7 @@
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 
 void main() {
-  final uri = Uri.parse('ws://localhost:8080/ws');
+  final uri = Uri.parse('ws://localhost:8080/ws_example');
   final channel = WebSocketChannel.connect(uri);
   channel.stream.listen((event) {
     print(event);
